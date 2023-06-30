131da96d98654eb44eb6a5d125bf144b3bd93dbc
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-06-29 21:09:41 -0300

    fix: using joi for data validation

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@ import express from "express";
 import cors from "cors";
 import { MongoClient, ObjectId } from "mongodb";
 import dayjs from "dayjs";
-import dotenv from 'dotenv';
+import joi from "joi";
+import dotenv from "dotenv";
 
 dotenv.config()
 
+/* API configuration */
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+/* Connection to mongoClient database */
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 async () => {
 
@@ -23,17 +26,30 @@ async () => {
 
 const db = mongoClient.db();
 
+/* Schemas for data validation */
+const participantSchema = joi.object({
+    name: joi.string().required()
+});
+
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid("message", "private_message").required()
+});
+
 app.post("/participants", async (req, res) => {
 
     try {
 
         const { name } = req.body;
 
-        if ((!name) || (typeof name !== "string")) {
+        const validation = participantSchema.validate({ name: name });
+
+        if (validation.error) {
             return res.sendStatus(422);
         }
 
-        const participantDB = await db.collection("participants").findOne({name: name});
+        const participantDB = await db.collection("participants").findOne({ name: name });
 
         /* Check if participant exists in database */
         if (participantDB) {
@@ -79,15 +95,19 @@ app.post("/messages", async (req, res) => {
         const { to, text, type } = req.body;
         const { user } = req.headers;
 
-        if ((!to || !text || !type) || (typeof to !== "string" || typeof text !== "string" || typeof type !== "string")) {
+        const message = {
+            to: to,
+            text: text,
+            type: type
+        };
+
+        const validation = messageSchema.validate(message, { abortEarly: false });
+
+        if (validation.error) {
             return res.sendStatus(422);
         }
 
-        if (type !== "message" && type !== "private_message") {
-            return res.sendStatus(422);
-        }
-
-        const participantDB = await db.collection("participants").findOne({name: user});
+        const participantDB = await db.collection("participants").findOne({ name: user });
 
         /* Check if participant exists in database */
         if (!participantDB) {
@@ -155,7 +175,7 @@ app.post("/status", async (req, res) => {
         const { user } = req.headers;
 
         if (!user) {
-            return res.send(404);
+            return res.sendStatus(404);
         }
 
         const userDB = await db.collection("participants").findOneAndUpdate(
@@ -208,15 +228,19 @@ app.put("/messages/:id", async (req, res) => {
         const { user } = req.headers;
         const { id } = req.params;
 
-        if ((!to || !text || !type) || (typeof to !== "string" || typeof text !== "string" || typeof type !== "string")) {
+        const message = {
+            to: to,
+            text: text,
+            type: type
+        };
+
+        const validation = messageSchema.validate(message, { abortEarly: false });
+
+        if (validation.error) {
             return res.sendStatus(422);
         }
 
-        if (type !== "message" && type !== "private_message") {
-            return res.sendStatus(422);
-        }
-
-        const participantDB = await db.collection("participants").findOne({name: user});
+        const participantDB = await db.collection("participants").findOne({ name: user });
 
         /* Check if participant exists in database */
         if (!participantDB) {
@@ -260,8 +284,7 @@ setInterval(async () => {
             
         inactiveparticipants.forEach(async (user) => {
 
-            await db.collection("participants").deleteOne({name: user.name});
-
+            await db.collection("participants").deleteOne({ name: user.name });
             await db.collection("messages").insertOne({
                 from: user.name,
                 to: 'Todos',
